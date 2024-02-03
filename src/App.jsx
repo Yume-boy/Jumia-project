fac8d5bafb7098eff02c9bec1399ec6f66080c35
Dakolo14 <ajosedare4u@gmail.com>
2024-02-03 19:52:05 +0100

    ajose commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Layout from './components/Layout';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
+import './App.css'
 
 
 const App = () => {
