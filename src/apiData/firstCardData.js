922ab172221b860546e797ac03af6bb9cae40dd0
iamjoel <olatigbejoel15@gmail.com>
2024-02-14 16:15:59 +0100

    Message

--- src/apiData/firstCardData.js
+++ src/apiData/firstCardData.js
@@ -5,55 +5,6 @@ const firstCard = [
     productName: "Clearance Sales"
   },
   {
-<<<<<<< HEAD
-    id: 2,
-    imagesrc: "..src/components/cards/images/artboard-10.png",
-    productName: "Best deals"
-  },
-  {
-    id: 3,
-    imagesrc: "..src/components/cards/images/artboard-1.png",
-    productName: "Phones & Tablets"
-  },
-  {
-    id: 4,
-    imagesrc: "..src/components/cards/images/artboard-2.png",
-    productName: "Computing Deals"
-  },
-  {
-    id: 5,
-    imagesrc: "..src/components/cards/images/artboard-3.png",
-    productName: "Groceries"
-  },
-  {
-    id: 6,
-    imagesrc: "..src/components/cards/images/artboard-4.png",
-    productName: "Televisions"
-  },
-  {
-    id: 7,
-    imagesrc: "..src/components/cards/images/artboard-5.png",
-    productName: "Men's Sneakers"
-  },
-  {
-    id: 8,
-    imagesrc: "..src/components/cards/images/artboard-6.png",
-    productName: "Mobile Accesories"
-  },
-  {
-    id: 9,
-    imagesrc: "..src/components/cards/images/artboard-7.png",
-    productName: "Generators"
-  },
-  {
-    id: 10,
-    imagesrc: "..src/components/cards/images/artboard-8.png",
-    productName: "Refrigerators"
-  },
-  {
-    id: 11,
-    imagesrc: "..src/components/cards/images/artboard-9.png",
-=======
     id: 11,
     imagesrc: "../src/components/cards/images/artboard-10.png",
     productName: "Best deals"
@@ -101,7 +52,6 @@ const firstCard = [
   {
     id: 10,
     imagesrc: "../src/components/cards/images/artboard-9.png",
->>>>>>> bafd12695ce46ef3407b3b29a3b1c09eabe0b5af
     productName: "Official store deals"
   },
   {
