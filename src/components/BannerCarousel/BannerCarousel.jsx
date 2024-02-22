b36bf5183635cdfdd7e1c73594790b3a45432f18
iamjoel <olatigbejoel15@gmail.com>
2024-02-22 16:16:54 +0100

    message

--- src/components/BannerCarousel/BannerCarousel.jsx
+++ src/components/BannerCarousel/BannerCarousel.jsx
@@ -59,6 +59,49 @@ const BannerCarousel = (props) => {
         <SwiperSlide><img src="https://ng.jumia.is/cms/0-1-initiatives/jbps/2024/CHI_LIMITED/CHI_Desktop_MLP_Slider__1168x384.jpg" alt="" /></SwiperSlide>
       </Swiper>
         </div>
+
+        <div className={`${props.show3}`}>
+          <div className={`bg-white d-none d-lg-block`}>
+          <h4>Official Stores</h4>
+            <div className={` mt-4 g-2 px-2`}>
+              <div className={`row col-lg py-3 `} >
+                <img style={{height:'110px', width:'185px', marginLeft:'5px'}} src="https://ng.jumia.is/cms/0-1-homepage/freelinks-brands-all/xiaomi_260x144.png" alt="" />
+
+                <img style={{height:'110px', width:'185px'}} src="https://ng.jumia.is/cms/0-1-homepage/freelinks-brands-all/nivea_260x144.png" alt="" />
+
+                <img style={{height:'110px', width:'185px'}} src="https://ng.jumia.is/cms/0-1-homepage/freelinks-brands-all/diageo_260x144.png" alt="" />
+
+                <img style={{height:'110px', width:'185px'}} src="https://ng.jumia.is/cms/0-1-homepage/freelinks-brands-all/oraimo_260x144.png" alt="" />
+
+                <img style={{height:'110px', width:'185px'}} src="https://ng.jumia.is/cms/0-1-homepage/freelinks-brands-all/adidas_260x144_V2.png" alt="" />
+
+                <img style={{height:'110px', width:'185px'}} src="https://ng.jumia.is/cms/0-1-homepage/freelinks-brands-all/reckitt_260x144.png" alt="" />
+              </div>
+            </div>
+          
+            <div className={``}>
+              <div className={`row col-lg py-3 `}>
+                <img style={{height:'110px', width:'185px', marginLeft:'5px'}} src="https://ng.jumia.is/cms/0-1-homepage/freelinks-brands-all/binatone_260x144.png" alt="" />
+
+                <img style={{height:'110px', width:'185px'}}  src="https://ng.jumia.is/cms/0-1-homepage/freelinks-brands-all/haier-thermocool_260x144.png" alt="" />
+
+                <img style={{height:'110px', width:'185px'}}  src="https://ng.jumia.is/cms/0-1-homepage/freelinks-brands-all/Brand-design-update/Pernod_ricard.png" alt="" />
+
+                <img style={{height:'110px', width:'185px'}}  src="https://ng.jumia.is/cms/0-1-homepage/freelinks-brands-all/infinix_260x144v2.png" alt="" />
+
+                <img style={{height:'110px', width:'185px'}}  src="https://ng.jumia.is/cms/0-1-homepage/freelinks-brands-all/tecno_260x144.png" alt="" />
+
+                <img style={{height:'110px', width:'185px'}}  src="https://ng.jumia.is/cms/0-1-homepage/freelinks-brands-all/bacardi-bacardi_260x144.png" alt="" />
+                
+              </div>
+            </div>
+
+            
+          </div>
+          <div className='d-lg-none'>
+
+          </div>
+        </div>
     </div>
   )
 }
